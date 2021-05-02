7b8e40da560390db0ae3414d2fda717d08d51a42
Cebrail Ceylan <cebraailc@gmail.com>
2021-05-02 17:25:17 +0300

    hoaxify hooks

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -6,9 +6,14 @@ export const signup = (body) => {
 
 export const login = creds => {
     {
-        return axios.post('/api/1.0/auth', {}, {auth: creds});
+        return axios.post('/api/1.0/auth', creds);
     }
 }
+
+export const logout = () => {
+    return axios.post('/api/1.0/logout')
+}
+
 export const changeLanguage = (language) => {
     //axiosın headerlerine bütün headerlerde ulaşacak ayar
     axios.defaults.headers["accept-language"] = language;
@@ -18,19 +23,17 @@ export const getUsers = (page = 0, size = 3) => {
     return axios.get(`/api/1.0/users?page=${page}&size=${size}`);
 }
 
-export const setAuthorizationHeader = ({username, password, isLoggedIn}) => {
+export const setAuthorizationHeader = ({isLoggedIn, token}) => {
     if (isLoggedIn) {
         //btoa username ve passwordubase64'e çevirir
         //username ve passwordu birleştirir
-        const authorizationHeaderValue = `Basic ${btoa(username + ':' + password)}`;
+        const authorizationHeaderValue = `Bearer ${token}`;
         axios.defaults.headers['Authorization'] = authorizationHeaderValue;
     } else {
         //kullanıcı çıkış yaptıktan sonra kullanıcılar listesinde adı görünecek
         delete axios.defaults.headers['Authorization']
     }
-
 }
-
 
 export const getUser = username => {
     return axios.get(`/api/1.0/users/${username}`)
--- src/redux/authActions.js
+++ src/redux/authActions.js
@@ -1,11 +1,17 @@
 import * as ACTIONS from "./Constans";
-import {login, signup} from "../api/apiCalls";
+import {login, logout, signup} from "../api/apiCalls";
 
 export const logoutSuccess = () => {
-    return {
-        type: ACTIONS.LOGOUT_SUCCESS
+    return async function(dispatch){
+        try {
+            await logout();
+        }catch (err) {
+
+        }
+        dispatch({type: ACTIONS.LOGOUT_SUCCESS})
     }
 }
+
 export const loginSuccess = (authState) => {
     return {
         type: ACTIONS.LOGIN_SUCCESS,
@@ -26,10 +32,10 @@ export const updateSuccess = ({displayName, image}) => {
 export const loginHandler = (credentials) => {
     return async function (dispatch) {
         const response = await login(credentials);
-
         const authState = {
-            ...response.data,
-            password: credentials.password
+            ...response.data.user,
+            password: credentials.password,
+            token:response.data.token
         }
         dispatch(loginSuccess(authState));
         //kullanıcı listesini getirirken giriş yapan kullanıcı getirmeyecek
